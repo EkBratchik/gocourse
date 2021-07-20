55a72e67a1800a2380a63e81d0ad4400377a444d
EkBratchik <71023472+EkBratchik@users.noreply.github.com>
2021-07-20 13:09:13 +0300

    Lesson 05 task

---
+++ lesson_05/main.go
@@ -0,0 +1,67 @@
+package main
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() (float64, error)
+	Perimetr() (float64, error)
+}
+func (s Circle) String() string{
+	return fmt.Sprintf("Circle: radius %.2f", s.radius)
+}
+func (s Rectangle) String() string{
+	return fmt.Sprintf("Rectangle with height %.2f and width %.2f", s.height,s.width)
+}
+type Circle struct {
+	radius float64
+}
+type Rectangle struct {
+	height float64
+	width float64
+}
+func (s Circle) Area() (float64, error){
+	if s.radius <= 0{
+		return 0, errors.New("Invalid value\n")
+	}
+	return math.Pi*math.Pow(s.radius,2), nil
+
+}
+func (s Rectangle) Area() (float64, error){
+	if s.height<=0 || s.width<=0{
+		return 0, errors.New("Invalid values\n")
+	}
+	return s.height*s.width, nil
+}
+func (s Circle) Perimetr() (float64, error){
+	if s.radius <= 0{
+		return 0, errors.New("Invalid value\n")
+	}
+	return 2*math.Pi*s.radius, nil
+}
+func (s Rectangle) Perimetr() (float64, error){
+	if s.width <= 0 || s.height <= 0{
+		return 0, errors.New("Invalid values\n")
+	}
+	return 2*s.height+2*s.width, nil
+}
+func DescribeShape(s Shape){
+	fmt.Println(s)
+	res,err := s.Area()
+	fmt.Printf("Area: %.2f ",res)
+	fmt.Println(err)
+	res,err = s.Perimetr()
+	fmt.Printf("Perimetr: %.2f ", res)
+	fmt.Println(err)
+}
+func main(){
+	c:=Circle{radius: 8}
+	r:=Rectangle{
+		height: 9,
+		width: 3,
+	}
+	DescribeShape(c)
+	DescribeShape(r)
+}
---
+++ lesson_05/main_test.go
@@ -0,0 +1,168 @@
+package main
+
+import "testing"
+
+func TestCircle_Area(t *testing.T) {
+	type fields struct {
+		radius float64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    float64
+		wantErr bool
+	}{
+		{
+			name: "return error if negative or null value",
+			fields: fields{radius: 0},
+			want: 0,
+			wantErr: true,
+		},
+		{
+			name: "return area of circle",
+			fields: fields{radius: 8},
+			want: 201.06192982974676,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Circle{
+				radius: tt.fields.radius,
+			}
+			got, err := s.Area()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Area() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Area() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCircle_Perimetr(t *testing.T) {
+	type fields struct {
+		radius float64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    float64
+		wantErr bool
+	}{
+		{
+			name: "return error if negative or null value",
+			fields: fields{radius: 0},
+			want: 0,
+			wantErr: true,
+		},
+		{
+			name: "return perimetr of circle",
+			fields: fields{radius: 8},
+			want: 50.26548245743669,
+			wantErr: false,
+		},
+	}
+		for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Circle{
+				radius: tt.fields.radius,
+			}
+			got, err := s.Perimetr()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Perimetr() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Perimetr() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRectangle_Area(t *testing.T) {
+	type fields struct {
+		height float64
+		width  float64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    float64
+		wantErr bool
+	}{
+		{
+			name: "error if negative or null values",
+			fields: fields{height: -1, width: -1},
+			want: 0,
+			wantErr: true,
+		},
+		{
+			name: "area of rectangle",
+			fields: fields{height: 9, width: 3},
+			want: 27,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Rectangle{
+				height: tt.fields.height,
+				width:  tt.fields.width,
+			}
+			got, err := s.Area()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Area() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Area() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRectangle_Perimetr(t *testing.T) {
+	type fields struct {
+		height float64
+		width  float64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    float64
+		wantErr bool
+	}{
+		{
+			name: "error if negative or null values",
+			fields: fields{height: -1, width: -1},
+			want: 0,
+			wantErr: true,
+		},
+		{
+			name: "perimetr of rectangle",
+			fields: fields{height: 9, width: 3},
+			want: 24,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Rectangle{
+				height: tt.fields.height,
+				width:  tt.fields.width,
+			}
+			got, err := s.Perimetr()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Perimetr() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Perimetr() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
